c46e8b5d466c8a108b82e2bf49f966200c9d8121
Dhekshith4113 <142972948+Dhekshith4113@users.noreply.github.com>
2023-08-23 10:49:18 +0000

    added

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+import weight_predicter
+
+app = Flask(__name__)
+
+@app.route('/', methods = ['GET', 'POST'])
+def hello():
+    if request.method == 'POST':
+        height = request.form['height']
+        print(height)
+        weight = weight_predicter.prediction([[float(height)]])
+        return render_template('index.html',ht = height, wt = weight)
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(port=8001)
